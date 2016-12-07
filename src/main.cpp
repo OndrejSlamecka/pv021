4025e39f0f14181de0f308e9ec09a79ce030cb7f
Filip Pokrývka <pokryva.f@czechglobe.cz>
2016-12-07 18:21:07 +0100

    Teach the network how to learn the BRCA dataset

--- src/main.cpp
+++ src/main.cpp
@@ -2,48 +2,128 @@
  * Copyright 2016 Michal Korbela, Filip Pokrývka, Ondřej Slámečka
  * See the LICENSE file for more information.
  */
-#include <iostream>
-#include <vector>
 
-#include "../mnist/include/mnist/mnist_reader.hpp"
+#include <bits/stdc++.h>
+
+#include "brca_image.hpp"
 
 #include "MLP.hpp"
 
-int main() {
-    MLP nn({2, 2, 1});
-    nn.randomize_weights(-1, 1);
+struct Stats {
+    int corr;
+    int unsure;
+    int fail;
+};
 
-    for (int i = 0; i < 10000; ++i) {
-        nn.learn({0, 0}, {0}, 0.5, 0.1);
-        nn.learn({0, 1}, {1}, 0.5, 0.1);
-        nn.learn({1, 0}, {1}, 0.5, 0.1);
-        nn.learn({1, 1}, {0}, 0.5, 0.1);
+std::vector<double> transform(bool i) {
+    std::vector<double> v;
+    if(i) {
+        v.push_back(0); v.push_back(1);
+    }
+    else {
+        v.push_back(1); v.push_back(0);
+    }
+    return v;
+}
+
+double validate(MLP &nn, std::vector<BrcaImage> &validation) {
+    Stats stats = {0,0,0};
+    std::vector<double> r1, r2;
+    for (unsigned i = 0; i<validation.size(); i++) {
+        r1 = nn.feed(validation[i].data);
+        if(r1[0]<0.5) r1[0] = 0;
+        else r1[0] = 1;
+        if(r1[1]<0.5) r1[1] = 0;
+        else r1[1] = 1;
+        if(r1 == transform(validation[i].malignant)) {
+            stats.corr++;
+        }
+        else if (r2 == transform(!validation[i].malignant) ){
+            stats.fail++;
+        }
+        else {
+            stats.unsure++;
+        }
+    }
+    std::cout << (stats.corr*100.0)/validation.size() << "%: " << stats.corr << "/" << validation.size() << " (" << stats.fail << " incorrect, " << stats.unsure << " unsure)\n";
+    return (stats.corr*100.0)/validation.size();
+}
+
+void normalize(std::vector<BrcaImage> &input, double a, double b) {
+    double min_input = input[0].data[0];
+    double max_input = input[0].data[0];
+    for (auto & img: input) 
+        for (auto & data: img.data) {
+            if (data<min_input)
+                min_input = data;
+            if (data>max_input)
+                max_input = data;
+        }
+    a = (max_input + min_input) / 2;
+    b = (max_input - min_input) / 2;
+    std::cout << "using normalization constants: (X-" << a << ")/" << b << "\n";
+    for (auto & img: input) 
+        for (auto & data: img.data)
+            data = (data-a)/b;
+
+    // test of normalisation
+    int bad = 0;
+    for (auto & img: input)
+        for (auto & data: img.data) {
+            if (data < -1 || data > 1)
+                bad++;
+        }
+    if (bad)
+        std::cout << "normalization went wrong, " << bad << " numbers are not in range <-1,1>\n";
+    else
+        std::cout << "data normalized successfully\n";
+}
+
+
+int main(int argc, char** argv) {
+    if (argc < 2)
+        throw std::invalid_argument("No data file specified\n"
+                                "USAGE: bin/nn traiing_file_name training_set_size"
+                                );
+    if (argc < 3)
+        throw std::invalid_argument("No training set size specified "
+                                "USAGE: bin/nn traiing_file_name training_set_size"
+                                );
+    
+    unsigned training_size;
+    
+    std::stringstream ss;
+    ss << argv[2];
+    ss >> training_size;
+
+    std::vector<BrcaImage> all_data = parse_brca_dataset(argv[1]);
+    double a=0, b=0;
+    normalize(all_data, a, b);
+    
+    if (training_size > all_data.size())
+        throw std::invalid_argument("please decrease training set size");
+
+    std::vector<BrcaImage> training, validation;
+    for(unsigned i = 0; i<training_size; ++i) 
+        training.push_back(all_data[i]);
+    
+    // how often is validation run
+    unsigned interval = 1000;
+    // when to stop
+    double treshold = 92.5;
+
+    MLP nn({30,2});
+    nn.randomize_weights(-1, 1);
+    int i = -1;
+
+    while (validate(nn, all_data) < treshold) {
+        ++i;
+        for (unsigned i = 0; i<interval; i++)
+            for (unsigned j = 0; j<training_size; j++)
+                nn.learn(training[j].data, transform(training[j].malignant), 0.5, 0.0);
     }
 
-    std::vector<double> r = nn.feed({1, 1});
-    std::cout << "exp 0, act " << r[0] << std::endl;
-
-    r = nn.feed({1, 0});
-    std::cout << "exp 1, act " << r[0] << std::endl;
-
-    r = nn.feed({0, 1});
-    std::cout << "exp 1, act " << r[0] << std::endl;
-
-    r = nn.feed({0, 0});
-    std::cout << "exp 0, act "  << r[0] << std::endl;
-
-
-    // auto dataset =
-    //     mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>();
-
-    // for (int i = 255; i --> 0; ) {
-        // if (dataset.training_images[5][i] > 0) {
-            // std::cout << i << std::endl;
-            // std::cout << dataset.training_images[5][i] << std::endl;
-            // break;
-        // }
-    // }
-
+    std::cout << "Learning finished, reached treshold " << treshold << "%" << std::endl;
 
     return 0;
 }
